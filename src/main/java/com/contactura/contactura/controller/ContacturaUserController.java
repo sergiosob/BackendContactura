119ee1d982a51fe72d0dbeba08c30ef7d7d4e973
sergiosob <sobrito@gmail.com>
2021-08-14 10:08:32 -0300

    criados model controller e repository para contactuser

---
+++ src/main/java/com/contactura/contactura/controller/ContacturaUserController.java
@@ -0,0 +1,73 @@
+package com.contactura.contactura.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.contactura.contactura.model.ContacturaUser;
+import com.contactura.contactura.repository.ContacturaUserRepository;
+
+@RestController
+@RequestMapping({"/user"}) //c
+public class ContacturaUserController {
+	
+	@Autowired
+	private ContacturaUserRepository repository;
+
+// List All - http://localhost:8095/contactura
+	@GetMapping
+	public List findAll() {
+		return repository.findAll();
+	}
+	
+// Find By Id - http://localhost:8095/contactura/{id}
+	@GetMapping(value = "{id]")
+	public ResponseEntity findById(@PathVariable long id) {
+		return repository.findById(id)
+				.map(record -> ResponseEntity.ok().body(record))
+				.orElse(ResponseEntity.notFound().build());
+	}
+		
+// Create -  http://localhost:8095/contactura/
+	@PostMapping
+	public ContacturaUser create(@RequestBody ContacturaUser user) {
+		return repository.save(user);
+		
+	}
+	
+// Update - http://localhost:8095/contactura/{id}
+	@PutMapping(value = "{id}")
+	public ResponseEntity<?> update(@PathVariable long id,
+			@RequestBody ContacturaUser user){
+			return repository.findById(id)
+				.map(record -> {
+					record.setUsername(user.getUsername());
+					record.setPassword(user.getPassword());
+					record.setName(user.getName());
+					record.setAdmin(true);
+					ContacturaUser update = repository.save(record);
+					return ResponseEntity.ok().body(update);
+				}).orElse(ResponseEntity.notFound().build());
+	}
+	
+//Delete - http://localhost:8095/contactura/{id}
+	@DeleteMapping(path = {"/{id}"})
+	public ResponseEntity<?> delete(@PathVariable long id){
+		return repository.findById(id)
+				.map(record -> {
+					repository.deleteById(id);
+					return ResponseEntity.ok().build();
+				}).orElse(ResponseEntity.notFound().build());
+	};
+
+	}
+
